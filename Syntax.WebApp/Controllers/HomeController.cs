5cba9581cc99ea4d7a73bb5c2f75797410a0f265
KitM4 <c_n1ksec@protonmail.com>
2024-02-10 15:14:01 +0200

    Add project files.

---
+++ Syntax.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Syntax.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Syntax.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Syntax.WebApp/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+
+app.Run();
