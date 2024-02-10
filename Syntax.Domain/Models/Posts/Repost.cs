00dfa948e820702bb4ed1c1eb7ab8ccf97453b41
KitM4 <c_n1ksec@protonmail.com>
2024-02-10 16:36:25 +0200

    Start of model implementation

---
+++ Syntax.Domain/Models/Posts/Repost.cs
@@ -0,0 +1,14 @@
+﻿namespace Syntax.Domain.Models.Posts;
+
+public class Repost
+{
+    public required Guid Id { get; set; }
+
+    public required string UserId { get; set; }
+    
+    public required virtual User User { get; set; }
+
+    public required Guid SnippetId { get; set; }
+
+    public required virtual Snippet Snippet { get; set; }
+}
