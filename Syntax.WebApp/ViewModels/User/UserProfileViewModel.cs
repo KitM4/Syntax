9b365b075edf3b1f30e9b9790298768095b20e8f
KitM4 <c_n1ksec@protonmail.com>
2024-02-14 20:45:21 +0200

    Implemented user page

---
+++ Syntax.WebApp/ViewModels/User/UserProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Syntax.Domain.Models.Posts;
+
+namespace Syntax.WebApp.ViewModels.User;
+
+public class UserProfileViewModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string? ProfileImageUrl { get; set; }
+
+    public string? Bio { get; set; }
+
+    public int SnippetsCount { get; set; } = 0;
+
+    public int FollowersCount { get; set; } = 0;
+
+    public int SubscriptionsCount { get; set; } = 0;
+
+    public ICollection<Snippet> Snippets { get; set; } = [];
+}
